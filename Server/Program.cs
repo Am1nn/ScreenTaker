e3d7bee1dbb8c0e8722e3cc0e1c8fddccdff77fa
Benna_cw18 <Benna_cw18@STHQ0123-03>
2023-12-04 16:45:12 +0400

    G

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,10 +1,51 @@
-﻿namespace Server
+﻿using System.Drawing.Imaging;
+using System.Drawing;
+using System.Net.Sockets;
+using System.Net;
+
+
+#pragma warning disable
+byte[] CaptureAndSaveScreenshot()
 {
-    internal class Program
+    int width = 1920;
+    int height = 1080;
+
+    using (Bitmap bmp = new Bitmap(width, height))
     {
-        static void Main(string[] args)
+        using (Graphics g = Graphics.FromImage(bmp))
         {
-            Console.WriteLine("Hello, World!");
+            g.CopyFromScreen(0, 0, 0, 0, bmp.Size);
+        }
+        using (MemoryStream stream = new MemoryStream())
+        {
+            bmp.Save(stream, ImageFormat.Jpeg);
+            return stream.ToArray();
         }
     }
+}
+
+
+
+UdpClient udpClient = new UdpClient(27001);
+var remoteEP = new IPEndPoint(IPAddress.Any, 0);
+while (true)
+{
+     var result = await udpClient.ReceiveAsync();
+    remoteEP = result.RemoteEndPoint;
+    new Task(async () =>
+    {
+        await Console.Out.WriteLineAsync(remoteEP.ToString());
+
+        var screen = CaptureAndSaveScreenshot();
+        var chunks = screen.Chunk(ushort.MaxValue - 29);
+
+        foreach (var chunk in chunks)
+        {
+            await udpClient.SendAsync(chunk, chunk.Length, remoteEP);
+        }
+
+    }).Start();
+
+
+
 }
